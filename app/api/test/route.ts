123b4efaa52fffb10e39fe693fa36364627e8dc2
Semih <shouldahjdh@gmail.com>
2025-04-21 03:54:30 +0300

    deleted the test api

--- app/api/test/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server"
-
-export const GET = async (req) => {
-    try {
-        
-    } catch (error) {
-        return NextResponse.json({error:"something went wrong"})
-    }
-}
